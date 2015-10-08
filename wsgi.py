f4d108b8f131f124a4b0c9db76fb07249a727f03
Andres MRM <andres@inventati.org>
2015-10-08 15:00:25 -0300

    OS wsgi

---
+++ wsgi.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+import os
+# import sys
+
+# this_path = "/patho/to/code"
+# sys.path.insert(0, this_path)
+
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+
+# os.chdir(this_path)
+
+from viralata.app import app as application
