8b8c0147330fae2dc6dfd0897ed353b76c1eca23
Andres MRM <andres@inventati.org>
2015-10-08 17:57:49 -0300

    allow set settings folder for manager

--- manage.py
+++ manage.py
@@ -8,14 +8,17 @@ from flask.ext.script import Server, Manager, Shell
 from viralata.app import create_app
 from viralata.extensions import db
 
-app = create_app(
-    os.path.join(
-        os.path.dirname(os.path.abspath(__file__)),
-        'settings'))
-manager = Manager(app)
-manager.add_command('run', Server(host="0.0.0.0", port=5002))
+
+manager = Manager(create_app)
+
+manager.add_option(
+    "-s", "--settings", dest="settings_folder", required=False,
+    default=os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'settings'))
+
+manager.add_command('run', Server(port=5002))
 manager.add_command('shell', Shell(make_context=lambda: {
-    'app': app,
+    'app': manager.app,
     'db': db,
 }))
 
